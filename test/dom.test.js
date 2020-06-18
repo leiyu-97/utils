c37d0b2fc6ac50e046a9709ff613434095ef0cb6
demonly <ly18824110284@gmail.com>
2020-06-18 16:54:16 +0800

    fix: 修复 dom.test 报错的问题

--- test/dom.test.js
+++ test/dom.test.js
@@ -1,40 +1,101 @@
-const jsdom = require('jsdom');
+/* eslint-disable no-undef */
 const assert = require('assert');
-const { isChildOf, isParentOf } = require('../src/dom');
+const puppeter = require('puppeteer');
+const rollup = require('rollup');
+const resolve = require('@rollup/plugin-node-resolve').default;
+const commonjs = require('@rollup/plugin-commonjs');
+const path = require('path');
 
-const { JSDOM } = jsdom;
-
-const domStr = `
-<div id="parent">
-  <div id="target">
-    <div id="child"></div>
-  </div>
-  <div id="sibling"></div>
-</div>
-`;
-
-const dom = new JSDOM(domStr);
-const { document } = dom.window;
-const parent = document.getElementById('parent');
-const child = document.getElementById('child');
-const sibling = document.getElementById('sibling');
-const target = document.getElementById('target');
-
-
-describe('dom', () => {
-  describe('isChildOf', () => {
-    it('结果正确', () => {
-      assert(isChildOf(target, parent));
-      assert(!isChildOf(target, child));
-      assert(!isChildOf(target, sibling));
-    });
+const init = async (html, scriptPath) => {
+  const bundle = await rollup.rollup({
+    input: scriptPath,
+    plugins: [resolve(), commonjs()],
   });
 
-  describe('isParentOf', () => {
-    it('结果正确', () => {
-      assert(!isParentOf(target, parent));
-      assert(isParentOf(target, child));
-      assert(!isParentOf(target, sibling));
+  const result = await bundle.generate({ format: 'iife', name: 'dom' });
+  const { code } = result.output[0];
+
+  const browser = await puppeter.launch();
+  const page = await browser.newPage();
+  page.setContent(html);
+  page.addScriptTag({ content: code });
+  return { browser, page };
+};
+
+describe('dom', async () => {
+  describe('isChildOf', async () => {
+    let browser;
+    let page;
+    const domStr = `
+    <div id="parent">
+      <div id="target">
+        <div id="child"></div>
+      </div>
+      <div id="sibling"></div>
+    </div>`;
+    before(async () => {
+      ({ browser, page } = await init(
+        domStr,
+        path.resolve(__dirname, '../src/dom/index.js'),
+      ));
     });
+
+    it('结果正确', async () => {
+      const result = await page.evaluate(() => {
+        const { isChildOf } = dom;
+        const parent = document.getElementById('parent');
+        const child = document.getElementById('child');
+        const sibling = document.getElementById('sibling');
+        const target = document.getElementById('target');
+        return [
+          isChildOf(target, parent),
+          !isChildOf(target, child),
+          !isChildOf(target, sibling),
+        ];
+      });
+      assert(result[0]);
+      assert(result[1]);
+      assert(result[2]);
+    });
+
+    after(async () => browser.close());
+  });
+
+  describe('isParentOf', async () => {
+    let browser;
+    let page;
+    const domStr = `
+    <div id="parent">
+      <div id="target">
+        <div id="child"></div>
+      </div>
+      <div id="sibling"></div>
+    </div>`;
+    before(async () => {
+      ({ browser, page } = await init(
+        domStr,
+        path.resolve(__dirname, '../src/dom/index.js'),
+      ));
+    });
+
+    it('结果正确', async () => {
+      const result = await page.evaluate(() => {
+        const { isParentOf } = dom;
+        const parent = document.getElementById('parent');
+        const child = document.getElementById('child');
+        const sibling = document.getElementById('sibling');
+        const target = document.getElementById('target');
+        return [
+          !isParentOf(target, parent),
+          isParentOf(target, child),
+          !isParentOf(target, sibling),
+        ];
+      });
+      assert(result[0]);
+      assert(result[1]);
+      assert(result[2]);
+    });
+
+    after(async () => browser.close());
   });
 });
