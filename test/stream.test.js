8ba620abe7f7ab9fbd0326cd0e639e0daa859f32
demonly <ly18824110284@gmail.com>
2020-07-03 17:05:53 +0800

    test: 添加 stream.combine 测试

--- test/stream.test.js
+++ test/stream.test.js
@@ -1,6 +1,7 @@
+/* eslint-disable max-classes-per-file */
 const assert = require('assert');
 const stream = require('stream');
-const { readAll } = require('../src/stream');
+const { readAll, combine } = require('../src/stream');
 
 class TestReadable extends stream.Readable {
   constructor(string) {
@@ -22,6 +23,31 @@ class TestReadable extends stream.Readable {
   }
 }
 
+class TestWriteable extends stream.Writable {
+  constructor() {
+    super();
+    this.data = Buffer.alloc(0);
+  }
+
+  _write(chunk, encoding, callback) {
+    this.data = Buffer.concat([this.data, chunk]);
+    callback();
+  }
+}
+
+class TestTransform extends stream.Transform {
+  constructor() {
+    super();
+    this.flow = 0;
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.push(chunk);
+    this.flow += chunk.length;
+    callback();
+  }
+}
+
 describe('stream', () => {
   describe('readAll', () => {
     it('正常工作', (done) => {
@@ -29,6 +55,32 @@ describe('stream', () => {
       readAll(testReadable).then((data) => {
         assert(data instanceof Buffer);
         assert(data.toString().length === 100000);
+        done();
+      });
+    });
+  });
+
+  describe('combine', () => {
+    it('拼接可读流和可写流正常工作', (done) => {
+      const testReadable = new TestReadable('0'.repeat(100000));
+      const testWriteable = new TestWriteable();
+      combine(testReadable, testWriteable);
+
+      testWriteable.on('finish', () => {
+        assert(testWriteable.data.toString().length === 100000);
+        done();
+      });
+    });
+
+    it('拼接转换流正常工作', (done) => {
+      const testReadable = new TestReadable('0'.repeat(100000));
+      const testTransform = new TestTransform();
+      const testWriteable = new TestWriteable();
+      combine(testReadable, testTransform, testWriteable);
+
+      testWriteable.on('finish', () => {
+        assert(testWriteable.data.toString().length === 100000);
+        assert(testTransform.flow === 100000);
         done();
       });
     });
