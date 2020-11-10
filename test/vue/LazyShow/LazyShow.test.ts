99d3d6891f73dce72dd4878c9b7758f74858f822
demonly <ly18824110284@gmail.com>
2020-11-10 21:18:25 +0800

    fix: 修复测试问题

--- test/vue/LazyShow/LazyShow.test.ts
+++ test/vue/LazyShow/LazyShow.test.ts
@@ -1,6 +1,6 @@
 import assert from 'power-assert'
 import { mount } from '@vue/test-utils'
-import LazyShow from '../../src/vue/LazyShow.vue'
+import LazyShow from '../../../src/vue/LazyShow.vue'
 
 describe('LazyShow', () => {
   test('正常挂载', () => {
